3648269823501d3ab5c2ff9fc8dd451368f197e4
ShubhsChavan <chavanshubham560@gmail.com>
2025-01-01 18:23:50 +0530

    diesease component designed

--- client/src/components/diseases/Diseases.jsx
+++ client/src/components/diseases/Diseases.jsx
@@ -5,13 +5,16 @@ import { useState } from 'react'
 import DiseaseModal from '../diseaseinfomodal/DiseaseInfoModal'
 const Diseases = () => {
     const diseaseList = [
-        { name: 'Kidney', imgSrc: '../../../assets/Images/kidney.png', description: 'शास्त्र शुद्ध पद्धतीने वजन का वाढत आहे याचा विचार करून वजन कमी होण्यासाठी औषधी दिली जातात.' },
-        { name: 'Kidney', imgSrc: '../../../assets/Images/kidney.png', description: 'शास्त्र शुद्ध पद्धतीने वजन का वाढत आहे याचा विचार करून वजन कमी होण्यासाठी औषधी दिली जातात.'  },
-        { name: 'त्वचेचे आजार', imgSrc: '../../../assets/Images/skin.png', description: 'शास्त्र शुद्ध पद्धतीने वजन का वाढत आहे याचा विचार करून वजन कमी होण्यासाठी औषधी दिली जातात.'  },
+        { name: 'उंची वाढवणे​', imgSrc: '../../../assets/Images/height2.png', description: 'शास्त्र शुद्ध पद्धतीने वजन का वाढत आहे याचा विचार करून वजन कमी होण्यासाठी औषधी दिली जातात.' },
+        { name: 'मधुमेह', imgSrc: '../../../assets/Images/diabetes.png', description: 'शास्त्र शुद्ध पद्धतीने वजन का वाढत आहे याचा विचार करून वजन कमी होण्यासाठी औषधी दिली जातात.'  },
+        { name: 'पचन संस्थेचे विकार', imgSrc: '../../../assets/Images/digestion.png', description: 'शास्त्र शुद्ध पद्धतीने वजन का वाढत आहे याचा विचार करून वजन कमी होण्यासाठी औषधी दिली जातात.'  },
         { name: 'त्वचेचे आजार', imgSrc: '../../../assets/Images/skin.png', description: 'शास्त्र शुद्ध पद्धतीने वजन का वाढत आहे याचा विचार करून वजन कमी होण्यासाठी औषधी दिली जातात.'  },
         { name: 'रक्ताचे आजार', imgSrc: '../../../assets/Images/blood.png', description: 'शास्त्र शुद्ध पद्धतीने वजन का वाढत आहे याचा विचार करून वजन कमी होण्यासाठी औषधी दिली जातात.' },
-        { name: 'रक्ताचे आजार', imgSrc: '../../../assets/Images/blood.png', description: 'शास्त्र शुद्ध पद्धतीने वजन का वाढत आहे याचा विचार करून वजन कमी होण्यासाठी औषधी दिली जातात.'   },
-        { name: 'लठ्ठपणा', imgSrc: '../../../assets/Images/obesity.png', description: 'शास्त्र शुद्ध पद्धतीने वजन का वाढत आहे याचा विचार करून वजन कमी होण्यासाठी औषधी दिली जातात.'  },
+        { name: 'स्त्री रोग व सौंदर्य समस्या', imgSrc: '../../../assets/Images/female.png', description: 'शास्त्र शुद्ध पद्धतीने वजन का वाढत आहे याचा विचार करून वजन कमी होण्यासाठी औषधी दिली जातात.'   },
+        { name: 'लठ्ठपणा', imgSrc: '../../../assets/Images/obesity2.png', description: 'शास्त्र शुद्ध पद्धतीने वजन का वाढत आहे याचा विचार करून वजन कमी होण्यासाठी औषधी दिली जातात.'  },
+        { name: 'अस्थमा', imgSrc: '../../../assets/Images/respiratory.png', description: 'शास्त्र शुद्ध पद्धतीने वजन का वाढत आहे याचा विचार करून वजन कमी होण्यासाठी औषधी दिली जातात.'  },
+        { name: 'सांधे दुखी', imgSrc: '../../../assets/Images/kneepain.png', description: 'शास्त्र शुद्ध पद्धतीने वजन का वाढत आहे याचा विचार करून वजन कमी होण्यासाठी औषधी दिली जातात.'  },
+        { name: 'थायरॉईड', imgSrc: '../../../assets/Images/thyroid.png', description: 'शास्त्र शुद्ध पद्धतीने वजन का वाढत आहे याचा विचार करून वजन कमी होण्यासाठी औषधी दिली जातात.'  },
         { name: 'लहान मुलांचे आजार', imgSrc: '../../../assets/Images/child.png', description: 'शास्त्र शुद्ध पद्धतीने वजन का वाढत आहे याचा विचार करून वजन कमी होण्यासाठी औषधी दिली जातात.'  },
 
     ]
